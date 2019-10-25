653184e1c1ff0e171ea2557db7cb3c60c139e698
Jonathan Behar <jdb443@stern.nyu.edu>
2019-10-24 21:50:49 -0400

    edit

--- models/index.js
+++ models/index.js
@@ -5,33 +5,37 @@ var path = require("path");
 var Sequelize = require("sequelize");
 var basename = path.basename(module.filename);
 var env = process.env.NODE_ENV || "development";
-<<<<<<< HEAD
 var config = require(__dirname + "/../config/config.json")[env];
-=======
-var config = require(__dirname + "/../config/connection.js")[env];
->>>>>>> 915b8c10e9ac2fec51788dac84a999a24302575c
 var db = {};
 
 if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+	var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+	var sequelize = new Sequelize(
+		config.database,
+		config.username,
+		config.password,
+		config
+	);
 }
 
-fs
-  .readdirSync(__dirname)
-  .filter(function(file) {
-    return (file.indexOf(".") !== 0) && (file !== basename) && (file.slice(-3) === ".js");
-  })
-  .forEach(function(file) {
-    var model = sequelize["import"](path.join(__dirname, file));
-    db[model.name] = model;
-  });
+fs.readdirSync(__dirname)
+	.filter(function(file) {
+		return (
+			file.indexOf(".") !== 0 &&
+			file !== basename &&
+			file.slice(-3) === ".js"
+		);
+	})
+	.forEach(function(file) {
+		var model = sequelize["import"](path.join(__dirname, file));
+		db[model.name] = model;
+	});
 
 Object.keys(db).forEach(function(modelName) {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
+	if (db[modelName].associate) {
+		db[modelName].associate(db);
+	}
 });
 
 db.sequelize = sequelize;
